66af18899560634ecd8783d69d502f52317226bf
Alignment Lab AI <130523883+Alignment-Lab-AI@users.noreply.github.com>
2023-06-02 14:14:01 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
-import numpy as np
+# tokenizer_utils.py
 from transformers import AutoTokenizer
 
-def get_tokenizer(model_path):
+def load_tokenizer(model_path):
     return AutoTokenizer.from_pretrained(model_path)
 
-def tokenize_text(tokenizer, input_text, output_path):
-    inputs = tokenizer.encode(input_text, return_tensors='pt')
-    np.save(output_path, inputs['input_ids'].numpy())
+def encode_input(tokenizer, input_text, device):
+    return tokenizer.encode(input_text, return_tensors='pt').to(device)
